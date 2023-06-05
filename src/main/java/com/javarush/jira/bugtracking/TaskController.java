95245fee1dde7d0a85475bf7b62f2c16763edc97
Anony2730677mous <104105@mail.ru>
2023-06-05 12:00:34 +0300

    tags_have_been_added_to_the_task

---
+++ src/main/java/com/javarush/jira/bugtracking/TaskController.java
@@ -0,0 +1,23 @@
+package com.javarush.jira.bugtracking;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping(value = "/api/tasks", produces = MediaType.APPLICATION_JSON_VALUE)
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping(value = "/{taskId}/tags", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void addTagsToTask(@PathVariable Long taskId, @RequestBody Set<String> tags) {
+        taskService.addTagsToTask(taskId, tags);
+    }
+}
--- src/main/java/com/javarush/jira/bugtracking/TaskService.java
+++ src/main/java/com/javarush/jira/bugtracking/TaskService.java
@@ -4,9 +4,11 @@ import com.javarush.jira.bugtracking.internal.mapper.TaskMapper;
 import com.javarush.jira.bugtracking.internal.model.Task;
 import com.javarush.jira.bugtracking.internal.repository.TaskRepository;
 import com.javarush.jira.bugtracking.to.TaskTo;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository> {
@@ -16,5 +18,13 @@ public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository
 
     public List<TaskTo> getAll() {
         return mapper.toToList(repository.getAll());
+    }
+
+    public void addTagsToTask(Long taskId, Set<String> tags) {
+        if (repository.existsById(taskId)){
+            Task task = repository.findById(taskId).get();
+            task.setTags(tags);
+            repository.save(task);
+        }
     }
 }
