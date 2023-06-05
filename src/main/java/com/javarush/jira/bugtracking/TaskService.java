607e3b4f129e3f75f93086ace9868048174bcb9c
Anony2730677mous <104105@mail.ru>
2023-06-05 12:01:10 +0300

    tags_have_been_added_to_the_task

--- src/main/java/com/javarush/jira/bugtracking/TaskService.java
+++ src/main/java/com/javarush/jira/bugtracking/TaskService.java
@@ -4,7 +4,6 @@ import com.javarush.jira.bugtracking.internal.mapper.TaskMapper;
 import com.javarush.jira.bugtracking.internal.model.Task;
 import com.javarush.jira.bugtracking.internal.repository.TaskRepository;
 import com.javarush.jira.bugtracking.to.TaskTo;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
